7da7eb306a990bde6afd27a8c9ea9904ecdfb3fb
MR.IBAY <ibayjimwell@gmail.com>
2025-01-08 09:24:55 +0800

    I adjust the Birthday label

--- frontend/src/components/DatePicker.vue
+++ frontend/src/components/DatePicker.vue
@@ -1,16 +1,18 @@
 <template>
-    <label htmlFor="name" class="block text-sm font-medium mb-2">Birthday</label>
-    <input 
-    type="date" 
-    id="birthday" 
-    name="birthday" 
-    placeholder="Enter your birthday" 
-    aria-label="Birthday" 
-    class="w-full py-3 px-4 border border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring focus:ring-blue-200"
-    :value="modelValue ? new Date(modelValue).toISOString().split('T')[0] : ''"
-    @input="$emit('update:modelValue', new Date($event.target.value).getTime())"
-    required
-    >
+    <div>
+        <label htmlFor="name" class="block text-sm font-medium mb-2">Birthday</label>
+        <input 
+        type="date" 
+        id="birthday" 
+        name="birthday" 
+        placeholder="Enter your birthday" 
+        aria-label="Birthday" 
+        class="w-full py-3 px-4 border border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring focus:ring-blue-200"
+        :value="modelValue ? new Date(modelValue).toISOString().split('T')[0] : ''"
+        @input="$emit('update:modelValue', new Date($event.target.value).getTime())"
+        required
+        >
+    </div>
 </template>
 
 <script>
